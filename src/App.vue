11d7ce44cf73921fe2d2575f703267a2e26347d4
王永瑶 <19374223@buaa.edu.cn>
2022-12-04 21:06:52 +0800

    git huai le

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
     <Header></Header>
     <router-view></router-view>
   </n-config-provider>
-
-<!--  <div style="display: block; height: 100%">-->
-<!--    <div v-if="headerVisiable" class="header-container">-->
-<!--      <Header-->
-<!--          :head_index="this.headerIndex"-->
-<!--      ></Header>-->
-<!--    </div>-->
-<!--    <div v-if="headerVisiable" style="height: 70px; background-color: rgb(243, 243, 244)"></div>-->
-<!--    <router-view :key="$route.fullPath"/>-->
-<!--  </div>-->
+  
 </template>
 
 <script>
