6e4851701a590d7a16ad2684ca9cddabb865b80f
卓乐 <zhuole@zhuoledeMacBook-Pro.local>
2022-12-15 00:27:16 +0800

    添加默认值

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
 module.exports = {
-  lintOnSave:false,  //关闭语法检查
+  devServer: {
+      overlay: {
+          warnings: false, //不显示警告
+          errors: false //不显示错误
+      }
+  },
+  lintOnSave: false //关闭eslint检查
 }
 
 // 跨域配置
@@ -14,7 +21,7 @@ module.exports = {
       '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
 
         // 后端的地址，通过 /api/xxx访问后端，弹幕有去掉api成功的
-        target: 'http://localhost:9999',     //代理的目标地址
+        target: 'http://localhost:8000',     //代理的目标地址
         changeOrigin: true,              //是否设置同源，输入是的
         pathRewrite: {                   //路径重写
           '^/api': ''                     //选择忽略拦截器里面的内容
