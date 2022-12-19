697d3cd7c9726c7447c1d5dfdf4673ac8fb3a6a3
lijinghao <lijinghao@buaa.edu.com>
2022-12-19 23:12:35 +0800

    详情页 增加收藏按钮

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -3,74 +3,132 @@
 
     <n-table :bordered="false" :single-line="false" size="small" class="opacity">
       <thead>
-      <n-text>{{ name }}</n-text>
+      <n-text>{{ info.name.value }}</n-text>
       </thead>
       <tbody>
       <tr>
         <td style="width: 200px" class="opacity">游戏ID</td>
-        <td style="width: 300px">{{ id }}</td>
+        <td style="width: 300px">{{ info.id.value }}</td>
       </tr>
       <tr>
         <td>标签</td>
-        <td>{{ tags }}</td>
+        <td>{{ info.tags.value }}</td>
       </tr>
       <tr>
         <td>发行商</td>
-        <td>{{ publisher }}</td>
+        <td>{{ info.publisher.value }}</td>
       </tr>
       <tr>
         <td>出版商</td>
-        <td>{{ publisher }}</td>
+        <td>{{ info.publisher.value }}</td>
+      </tr>
+      <tr>
+        <td>平台</td>
+        <td>{{ info.platform.value }}</td>
       </tr>
       <tr>
         <td>发行日期</td>
-        <td>{{ time }}</td>
+        <td>{{ info.time.value }}</td>
       </tr>
       </tbody>
     </n-table>
 
     <!--宽设置统一-->
-    <n-image height="160" src="https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1668885944"/>
+    <n-space vertical>
+      <n-image height="160" :src="info.image.value"/>
+
+      <n-button type="primary" @click="handleFavor">
+        {{ favorButtonRef.text }}
+      </n-button>
+    </n-space>
   </n-space>
 </template>
 
 <script>
 import {useRouter} from "vue-router/dist/vue-router";
 import request from "@/utils/request";
+import {onMounted, ref} from "vue";
+import store from "@/store";
 
 export default {
   name: "BasicInfo",
   setup() {
     const router = useRouter();
+    const favorButtonRef = ref({
+      "text": "收藏",
+      "flag": false,
+    })
 
     const gameId = parseInt(router.currentRoute.value.params.gameid);//游戏id
+    let info = {
+      "id": ref(''),
+      "name": ref(''),
+      "platform": ref(''),
+      "publisher": ref(''),
+      "time": ref(''),
+      "tags": ref(''),
+      "image": ref(''),
+    };
 
-    let data;
-    //TODO 发送游戏id，得到游戏的id、游戏名、发行商、发行日期、标签
-    // request.post('/getGame/', JSON.stringify({'id': gameId})).then(res => {
-    //   data = res.data;
-    // })
-    console.log(data);
-    let publisher = '游戏的发行商';
-    let id = '游戏的id号';
-    let name = '游戏名';
-    let time = '游戏的发行日期';
-    let tags = "射击 策略";
+    onMounted(() => {
+      // 发送游戏id，得到游戏的id、游戏名、发行商、发行日期、标签
+      request.post('/getGame/', JSON.stringify({'game_id': gameId})).then(res => {
+        // console.log(res.data[0]);
+        let gameInfo = res.data[0]
+        info.id.value = gameInfo.id;
+        info.name.value = gameInfo.name;
+        info.platform.value = gameInfo.platform;
+        info.publisher.value = gameInfo.developer;
+        info.time.value = gameInfo.date;
+        info.tags.value = gameInfo.tag;
+        info.image.value = gameInfo.cover;
+      });
+
+      let post = JSON.stringify({
+        "game_id": gameId,
+        "user_id": store.state.user.userID,
+      });
+      // request.post("/用户是否收藏了这个游戏/", post).then(res => {
+        //TODO 根据res得到是否被收藏
+        favorButtonRef.value.flag = true;
+        if (favorButtonRef.value.flag) {
+          favorButtonRef.value.text = "取消收藏";
+        } else {
+          favorButtonRef.value.text = "收藏";
+        }
+      // })
+    })
+
+    function handleFavor() {
+      let post = JSON.stringify({
+        "game_id": gameId,
+        "user_id": store.state.user.userID,
+      });
+      if (favorButtonRef.value.flag) {
+        // request.post('取消收藏', post).then(res => {
+          favorButtonRef.value.flag = false;
+          favorButtonRef.value.text = "收藏";
+        // })
+      } else {
+        // request.post('收藏', post).then(res => {
+          favorButtonRef.value.flag = true;
+          favorButtonRef.value.text = "取消收藏";
+        // })
+      }
+    }
 
     return {
-      id,
-      name,
-      publisher,
-      time,
-      tags,
+      info,
+      favorButtonRef,
+      handleFavor,
     };
   }
 }
 </script>
 
 <style scoped>
-.opacity{
-  background-color: rgba(255,0,0,0);
+.opacity {
+  background-color: rgba(255, 0, 0, 0);
   /*opacity: 0;*/
 }
 </style>
