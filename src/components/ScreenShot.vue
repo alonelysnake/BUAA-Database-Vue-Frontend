d1f0611a096d990663a6cf5dce051f4389613b01
lijinghao <lijinghao@buaa.edu.com>
2022-12-20 18:23:48 +0800

    详情页 游戏截图前后端交互

--- src/components/ScreenShot.vue
+++ src/components/ScreenShot.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="screenshot">
     <n-grid :cols="2">
-      <n-gi v-for="img in imgs" :key="img.id" class="img-container">
-        <n-image width="700" :src="img.addr"/>
+      <n-gi v-for="img in imgs" :key="img.game_id" class="img-container">
+        <n-image width="600" :src="img.url"/>
       </n-gi>
     </n-grid>
   </div>
@@ -11,6 +11,7 @@
 <script>
 import {useRouter} from "vue-router";
 import request from "@/utils/request";
+import {ref} from "vue";
 
 export default {
   name: "ScreenShot",
@@ -19,15 +20,10 @@ export default {
     const router = useRouter();
     const gameId = parseInt(router.currentRoute.value.params.gameid);
     //TODO 根据游戏id获取所有图片
-    let imgs = [];
-    request.post("/getScreenShot/", JSON.stringify({"id": gameId})).then(res => {
-      imgs = res.data;
+    let imgs = ref([]);
+    request.post("/getGamePhoto/", JSON.stringify({"game_id": gameId})).then(res => {
+      imgs.value = res.data;
     });
-    imgs = [
-      {id: 0, addr: "https://cdn.cloudflare.steamstatic.com/steam/apps/1599340/header.jpg?t=1670026493"},
-      {id: 0, addr: "https://cdn.cloudflare.steamstatic.com/steam/apps/1599340/header.jpg?t=1670026493"},
-      {id: 0, addr: "https://cdn.cloudflare.steamstatic.com/steam/apps/1599340/header.jpg?t=1670026493"},
-    ]
 
     return {
       imgs,
