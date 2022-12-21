3c87a5be51e453310b95ba0817fdc5ef79be2642
卓乐 <zhuole@zhuoledeMacBook-Pro.local>
2022-12-21 23:00:17 +0800

    新增条件筛选

--- back/backend/admin.py
+++ back/backend/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register([User, Game, Goods])
+admin.site.register([User, Game, Goods, Heat, Tag, GamePhoto, Favorites, Developer, Develop, Country, Price, Discount, GameDiscount, Comment, News, Slide])
